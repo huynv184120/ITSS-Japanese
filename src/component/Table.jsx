e96a5ca7366ed6d8d9bc0ea32597e83ea57eec18
ThangDao0611 <daomanhthang0611@gmail.com>
2022-07-20 22:03:31 +0700

    feat: connect api

--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -8,13 +8,8 @@ import {
     Legend,
     Bar,
 } from "recharts";
-import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from 'axios';
-
-const mockData = [{"personId":23,"email":"nguyenngocaocuong@gmail.com","location":{"locationId":1,"locationName":"Nghệ An"},"name":null,"age":21,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":12,"freeTimeAction":1,"password":"password","isAdmin":true},{"personId":24,"email":"nguyendinhcanh@gmail.com","location":{"locationId":2,"locationName":"Thanh Hóa"},"name":null,"age":22,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":12,"freeTimeAction":3,"password":"password","isAdmin":false},{"personId":25,"email":"lebaohieu@gmail.com","location":{"locationId":2,"locationName":"Thanh Hóa"},"name":null,"age":18,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":12,"freeTimeAction":5,"password":"password","isAdmin":false},{"personId":26,"email":"tadanghuy@gmail.com","location":{"locationId":2,"locationName":"Thanh Hóa"},"name":null,"age":16,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":11,"freeTimeAction":2,"password":"cuongcuong","isAdmin":false},{"personId":27,"email":"daoduythai@gmail.com","location":{"locationId":3,"locationName":"Hà Tĩnh"},"name":null,"age":32,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":10,"freeTimeAction":5,"password":"password","isAdmin":false},{"personId":28,"email":"lethaitong@gmail.com","location":{"locationId":3,"locationName":"Hà Tĩnh"},"name":null,"age":23,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":1,"freeTimeAction":6,"password":"password","isAdmin":false},{"personId":29,"email":"hadaihanh@gmail.com","location":{"locationId":5,"locationName":"Bình Dương"},"name":null,"age":16,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":7,"freeTimeAction":8,"password":"password","isAdmin":false},{"personId":30,"email":"dongthamthuc@gmail.com","location":{"locationId":5,"locationName":"Bình Dương"},"name":null,"age":17,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":12,"freeTimeAction":9,"password":"password","isAdmin":false},{"personId":31,"email":"toquymac@gmail.com","location":{"locationId":7,"locationName":"Hải Dường"},"name":null,"age":27,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":7,"freeTimeAction":5,"password":"password","isAdmin":false},{"personId":32,"email":"anhtuha@gmail.com","location":{"locationId":8,"locationName":"Thái Bình"},"name":null,"age":32,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":8,"freeTimeAction":2,"password":"cuongcuong","isAdmin":false},{"personId":33,"email":"nguyenduclam@gmail.com","location":{"locationId":7,"locationName":"Hải Dường"},"name":null,"age":18,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0855050635","sex":null,"religion":null,"education":1,"createTime":3,"freeTimeAction":3,"password":"cuongcuong","isAdmin":false},{"personId":34,"email":"phamhongson@gmail.com","location":{"locationId":5,"locationName":"Bình Dương"},"name":null,"age":18,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":4,"freeTimeAction":4,"password":"cuongcuong","isAdmin":false},{"personId":35,"email":"nguyenvanlong@gmail.com","location":{"locationId":6,"locationName":"Hà Nội"},"name":null,"age":19,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":4,"freeTimeAction":4,"password":"cuongcuong","isAdmin":false},{"personId":36,"email":"truongsyhung@gmail.com","location":{"locationId":4,"locationName":"Ninh Bình"},"name":null,"age":26,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":6,"freeTimeAction":7,"password":"cuongcuong","isAdmin":false},{"personId":37,"email":"lathevinh@gmail.com","location":{"locationId":6,"locationName":"Hà Nội"},"name":null,"age":29,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":6,"freeTimeAction":7,"password":"cuongcuong","isAdmin":false},{"personId":38,"email":"dangthaimai@gmail.com","location":{"locationId":2,"locationName":"Thanh Hóa"},"name":null,"age":33,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":3,"freeTimeAction":8,"password":"cuongcuong","isAdmin":false},{"personId":39,"email":"songkhongtu@gmail.com","location":{"locationId":2,"locationName":"Thanh Hóa"},"name":null,"age":31,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":7,"freeTimeAction":6,"password":"cuongcuong","isAdmin":false},{"personId":40,"email":"hongbemac@gmail.com","location":{"locationId":1,"locationName":"Nghệ An"},"name":null,"age":35,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":8,"freeTimeAction":6,"password":"cuongcuong","isAdmin":false},{"personId":41,"email":"dangsuthe@gmail.com","location":{"locationId":1,"locationName":"Nghệ An"},"name":null,"age":16,"image":null,"avatar":"url","height":12.3,"phoneNumber":"0818988577","sex":null,"religion":null,"education":1,"createTime":2,"freeTimeAction":6,"password":"cuongcuong","isAdmin":false}];
-
-const data = [[4,2],[7,3],[10,1],[6,2],[12,4],[3,2],[1,1],[2,1],[8,2],[11,1]];
 
 export const Table = ({ type }) => {
 
@@ -24,9 +19,8 @@ export const Table = ({ type }) => {
     useEffect(() => {
         axios.get(`https://app-matching-friend.herokuapp.com/accounts`)
         .then(res => {
-            console.log(res)
-            // this.setState({ persons });
-            setList(res)
+            console.log("user", res)
+            setList(res.data)
         })
         .catch(error => console.log(error));
     }, [])
@@ -34,35 +28,25 @@ export const Table = ({ type }) => {
     useEffect(() => {
         axios.get(`https://app-matching-friend.herokuapp.com/features/avg`)
         .then(res => {
-            console.log(res)
-            const resData = data.map((arr)=>{
+            console.log("avg", res)
+            const arr = res.data.map((arr)=>{
                 return {
                     'month': arr[0],
                     'avg': arr[1]
                 }
             })
-            console.log(resData)
-            setListAvg(res)
-        })
-        .catch(error => {
-            let resData = data.map((arr)=>{
-                return {
-                    'month': arr[0],
-                    'avg': arr[1]
-                }
-            })
-            resData.sort((a, b)=>{
+            arr.sort((a, b)=>{
                 return a.month - b.month
             })
-            setListAvg(resData)
-        });
+            setListAvg(arr)
+        })
+        .catch(error => console.log(error));
     }, [])
 
     const handleRemove = (id) =>{
         if (window.confirm("Delete the user?")) {
-            console.log(id)
-            // axios.delete(`https://app-matching-friend.herokuapp.com/accounts/delete-account/${id}`)
-            axios.delete(`https://app-matching-friend.herokuapp.com/accounts/delete-account/1`)
+            axios.delete(`https://app-matching-friend.herokuapp.com/accounts/delete-account/${id}`)
+            setList(listUser.filter(user => user.personId !== id))
         }
     }
 
@@ -81,7 +65,7 @@ export const Table = ({ type }) => {
                     <ul
                         className="divide-y divide-gray-200 dark:divide-gray-700"
                     >
-                        {mockData.map((obj, i) => (
+                        {listUser.map((obj, i) => (
                             <li className="py-3 sm:py-4 bg-gray-100 px-3 rounded-md shadow-lg mb-5">
                                 <div className="flex items-center space-x-4">
                                     <div className="flex-shrink-0">
@@ -98,6 +82,15 @@ export const Table = ({ type }) => {
                                         <p className="text-sm text-blue-600 truncate dark:text-gray-200">
                                             {obj.email}
                                         </p>
+                                        <div class="relative flex flex-col items-center group">
+                                            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+                                            </svg>
+                                            <div class="absolute bottom-0 flex flex-col items-center hidden mb-6 group-hover:flex">
+                                                <span class="relative z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-black shadow-lg">{obj.email}</span>
+                                                <div class="w-3 h-3 -mt-2 rotate-45 bg-black"></div>
+                                            </div>  
+                                        </div>
                                     </div>
                                     <div className="flex-1 min-w-0">
                                         <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
