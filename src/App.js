cea99d8526ca5f5667454fd365ab31b690f253b1
huynv184120 <nguyenviethuy2403@gmail.com>
2022-07-20 14:45:02 +0700

    base

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { LoginForm } from './component/LoginForm'
 import { Profile } from './component/Profile'
 import { SignupForm } from './component/SignupForm'
 import { UserHome } from './component/UserHome'
-import { Chat } from './page/Chat'
+import Chat  from './Chat'
 import { Home } from './page/Home'
 import { Login } from './page/Login'
 import { PageNotFound } from './page/PageNotFound'
@@ -25,7 +25,7 @@ function App() {
             <Route path='' element={<UserHome />} />
             <Route path='profile' element={<Profile />} />
           </Route>
-          <Route path='chat/:id' element={<Chat />} />
+          <Route path='chat' element={<Chat />} />
         </Route>
         <Route path='login' element={<Login />}>
           <Route path='login-form' element={<LoginForm />} />
